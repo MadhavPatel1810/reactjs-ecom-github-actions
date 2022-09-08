b839e858373e300773ef49c5e0db0a0c2d337470
madhavpatel <madhavpatel1810@gmail.com>
2022-09-08 15:19:36 +0530

    Upload Final commit on 08-09-2022

--- src/Components/modules/Home/index.js
+++ src/Components/modules/Home/index.js
@@ -6,8 +6,8 @@ import ProductTypes from "./component/ProductTypes";
 export const Home = () => {
   return (
     <div className="container-fluid p-0 mx-50">
-      <h2>React CI/CD</h2>
       <ProductSlider home="home" />
+      <h2>React CI/CD</h2>
       <ProductTypes card="card" />
       <Products />
     </div>
