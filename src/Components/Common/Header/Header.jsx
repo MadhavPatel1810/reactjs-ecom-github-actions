77acfac900583fc67c7f7eb7f3dbd914c40fe6e3
Madhav Patel <86973682+MadhavPatel1810@users.noreply.github.com>
2022-11-04 16:53:34 +0530

    Update Header.jsx

--- src/Components/Common/Header/Header.jsx
+++ src/Components/Common/Header/Header.jsx
@@ -77,7 +77,6 @@ export const Header = () => {
         }
       >
         <div className="container ">
-          <p>hmmmmm</p>
           <Link className="navbar-brand" to="#">
             <img
               src={navLogo}
